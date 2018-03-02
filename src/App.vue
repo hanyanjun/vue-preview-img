fdc1a5e012f07d0ffd593028b9d4cb64c3b29373
Hardy <HYJ_hanyanjun@163.com>
2018-03-02 16:00:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <img :src="`https://www.hanyanjun.top/static/image/${item}.jpg`" alt="" v-for="item in  imgs">
+    <previewImg></previewImg>
+  </div>
+</template>
+
+<script>
+import previewImg from './components/preview-img'
+
+export default {
+  name: 'App',
+  data(){return{
+     imgs : ['2','3','4','5','6']
+  }},
+  components: {
+    previewImg
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  img{
+    display: inline-block;
+    width: 100px;
+    height: 100px;
+    margin-right: 30px;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/components/preview-img.vue
@@ -0,0 +1,12 @@
+<template>
+
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+</style>
