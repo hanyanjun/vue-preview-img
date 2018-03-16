8fb55a5bdd076221870658a167c38771442bec4e
Hardy <HYJ_hanyanjun@163.com>
2018-03-16 18:11:27 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <div id="hw-preview_img_wrap" v-show="visible || img">
+  <div id="hw-preview_img_wrap">
     <h6 class="title">{{title}}</h6>
     <i class="icon-close pointer"></i>
     <div class="pre_img_content">
       <div class="pre_img_con_cur">
-        <i class="icon icon-arrow-l"></i>
-        <div class="pre_img_cur">
-
+        <i class="icon icon-arrow-l" @click.stop="pre"></i>
+        <div :class="['pre_img_cur',{'pre_img_cur_move':is_move}]"  ref="pre_img_content">
+           <preview-img
+             :url="url"
+             :scale="scale"
+             :rotate="rotate"
+             :top.sync="top"
+             :left.sync="left"
+             :preImgCss ="trans"
+             :initWidth.sync="i_w"
+             :i_top.sync="i_top"
+             :i_left.sync="i_left"
+             :initHeight.sync="i_h"
+             :initTop.sync="initTop"
+             :updatePos.sync="updatePos"
+             :initLeft.sync="initLeft">
+             <!--<i class="loading_icon icon-load1" slot="load"></i>-->
+           </preview-img>
         </div>
-        <i class="icon icon-arrow-r"></i>
+        <i class="icon icon-arrow-r" @click.stop="suf"></i>
       </div>
     </div>
     <div :class="['pre_img_con_fn',{pre_img_con_fn_active :imgs_srpead }]">
@@ -27,16 +42,16 @@
       <span class="fn_module pointer">
            <i class="icon icon-play"></i>
             自动播放
-       </span><span class="fn_module pointer">
+       </span><span class="fn_module pointer" @click.stop="bigger">
            <i class="icon icon-scale-up"></i>
             放大
-       </span><span class="fn_module pointer">
+       </span><span class="fn_module pointer" @click.stop="smaller">
            <i class="icon icon-scale-down"></i>
             缩小
-       </span><span class="fn_module pointer">
+       </span><span class="fn_module pointer" @click.stop="spin">
            <i class="icon icon-rotate"></i>
             旋转
-       </span><span class="fn_module pointer">
+       </span><span class="fn_module pointer" @click.stop="init">
            <i class="icon icon-init"></i>
             复位
        </span>
@@ -44,78 +59,251 @@
     <div class="pre_img_con_list" v-show="imgs_srpead">
       <i class="icon pointer icon-arrow-l"></i>
       <i class="icon pointer icon-arrow-r"></i>
-       <span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
-       </span><span class="img">
-
+       <span :class="['img',{'img_active':index == count}]" v-for="(item,index) in urls">
+           <preview-img :url="item" @select="select">
+             <i class="loading_icon icon-load" slot="load"></i>
+           </preview-img>
        </span>
     </div>
-    <hwLoad></hwLoad>
   </div>
 </template>
 
 <script>
 import hwImg from "./components/hw-img.vue"
 import hwLoad from "./components/hw-load.vue"
+import previewImg from "./components/preview-img.vue"
 export default {
   name: 'App',
   data(){return{
     imgs_srpead : false, //图片展开收起状态
+    init_rotate : this.rotate, //初始旋转角度
+    init_scale : this.scale, //初始缩放比例
+    init_pre_img_css : this.preImgCss, //初始css样式
+    w : '', //父容器宽度
+    h : '', //父容易高度
+    i_w : 0, //图片本身宽度
+    i_h : 0, //图片本身高度
+    sX : 0, //鼠标按下初始x坐标
+    sY : 0, //鼠标按下初始y坐标
+    i_top : 0, //初始化时图片top值
+    i_left : 0, //初始化时图片left值
+    initTop : 0, //每次移动的基础top值
+    initLeft : 0, //每次移动的基础left值
+    top : 0, //需要更改的预览图片的top值
+    left : 0, //需要更改的预览图片的left值
+    updatePos : false //更新图片位置信息
   }},
   components: {
-      hwImg,hwLoad
+      hwImg,hwLoad,previewImg
+  },
+  mounted(){
+      this.imgs_srpead = true;
+      this.w = parseInt(this.$refs.pre_img_content.offsetWidth);
+      this.h = parseInt(this.$refs.pre_img_content.offsetHeight);
+      setTimeout(()=>{
+        this.$emit('update:url','https://www.hanyanjun.top/static/image/6.jpg');
+      },1000);
+  },
+  watch:{
+    i_top(v){
+        this.top = v;
+    },
+    i_left(v){
+      this.left = v;
+    },
+  },
+  computed:{
+    count(){
+      let c = '-2';
+      this.urls.forEach((v1,i)=>{
+        if(v1 == this.url && this.url){
+          c = i;
+          return i;
+        }
+      })
+      return c;
+    },
+    trans(){
+      return {transform : `rotateZ(${this.rotate}deg) scale(${this.scale})`, marginTop : `${this.top}px` , marginLeft : `${this.left}px`, cursor: `${this.is_move ? 'move' : 'normal'}` };
+    },
+    is_move(){
+      let m = false;
+      if(this.rotate/90%2 == 0){
+        m = this.i_w*this.scale > this.w || this.i_h*this.scale > this.h;
+      }else{
+        m = this.i_h*this.scale > this.w || this.i_w*this.scale > this.h;
+      }
+      m = true;
+      this.$nextTick(()=>{
+        if(m){
+          this.$refs.pre_img_content.addEventListener('mousedown',this.touchstart,false);
+        }else{
+          this.top = this.i_top;
+          this.left = this.i_left;
+          this.$refs.pre_img_content.removeEventListener('mousedown',this.touchstart,false);
+        }
+      })
+      return m;
+    },
+    maxT(){
+//        可移动是在宽度或者高度溢出时
+      let max = 0;
+      if(this.rotate/90%2 == 0){
+        max = (this.i_h*this.scale - this.h)/2 + this.h  - this.moveMinWidth ;
+      }else{
+        max = (this.i_w*this.scale - this.h)/2 + this.h  - this.moveMinWidth;
+      }
+      return max;
+    },
+    maxL(){
+//        可移动是在宽度或者高度溢出时
+      let max = 0;
+      if(this.rotate/90%2 == 0){
+        max = this.i_w*this.scale  + this.i_left - this.moveMinWidth;
+      }else{
+        max = (this.i_h*this.scale - this.w)/2 + this.w  - this.moveMinWidth;
+      }
+      console.log(max);
+      return max;
+    },
   },
   props:{
       title :{
           type : String,
           default : '图片预览'
       },
-      imgs:{
+      urls:{
           type : Array,
           default : function () {
             return ['https://www.hanyanjun.top/static/image/2.jpg','https://www.hanyanjun.top/static/image/3.jpg','https://www.hanyanjun.top/static/image/4.jpg','https://www.hanyanjun.top/static/image/5.jpg','https://www.hanyanjun.top/static/image/6.jpg']
           }
       },
-      img:{
+      url:{
         type : String,
-        default : ''
+        default : 'https://www.hanyanjun.top/static/image/6.jpg'
       },
       visible:{
           type : Boolean,
           default : true
+      },
+      rotate:{
+          type: Number,
+          default: 0
+      },
+      scale :{
+          type : Number,
+          default : 1
+      },
+      maxScale:{
+          type : Number,
+          default : 4
+      },
+      preImgCss:{
+          type : Object,
+          default : function () {
+            return {}
+          }
+      },
+      moveMinWidth : {
+        type : Number,
+        default : 30
+      },
+      moveMinHeight : {
+        type : Number,
+        default : 30
+      },
+  },
+  methods:{
+    select(v){
+        this.$emit('update:url',v);
+        this.$emit('select',v);
+        this.url = v;
+        this.init();
+    },
+    pre(v){
+      if(this.count > 0){
+        this.$emit('update:url',this.urls[this.count-1]);
+        this.$emit('pre',this.urls[this.count-1]);
+        this.url = this.urls[this.count-1];
+        this.init();
+      }else{
+        this.$emit('suf','min');
       }
+    },
+    suf(v){
+      if(this.count < this.urls.length - 1){
+        this.$emit('update:url',this.urls[this.count+1]);
+        this.$emit('suf',this.urls[this.count+1]);
+        this.url = this.urls[this.count+1];
+        this.init();
+      }else{
+        this.$emit('suf','max');
+      }
+    },
+    bigger(){
+      let s = this.scale;
+      s += 0.3;
+      if(s > this.maxScale){
+        s = 4;
+      }
+      this.scale = s;
+      this.$emit('update:scale',s);
+    },
+    smaller(){
+      let s = this.scale;
+      s -= 0.3;
+      if(s < 0.3){
+        s = 0.3;
+      }
+      this.scale = s;
+      this.$emit('update:scale',s);
+
+    },
+    spin(){
+      let r = this.rotate;
+      r += 90;
+      this.rotate = r;
+      this.$emit('update:rotate',r);
+    },
+    init(){
+      this.rotate = this.init_rotate;
+      this.scale = this.init_scale;
+      this.preImgCss = this.init_pre_img_css;
+      this.$emit('update:rotate',this.init_rotate);
+      this.$emit('update:preImgCss',this.init_pre_img_css);
+      this.$emit('update:scale',this.init_scale);
+    },
+    touchstart(e){
+      e = e || window.event;
+      e.preventDefault();
+      this.sY = parseInt(e.clientY);
+      this.sX = parseInt(e.clientX);
+      this.updatePos = true; //更新图片位置信息
+      document.addEventListener('mousemove',this.touchmove,false);
+      document.addEventListener('mouseup',this.tEventUp,false);
+    },
+    touchmove(e){
+      e = e || window.event;
+      e.preventDefault();
+      let y = parseInt(e.clientY);
+      let x = parseInt(e.clientX);
+      let maxT = this.maxT;
+      let maxL = this.maxL;
+      console.log(this.initLeft);
+      if(y <= this.sY){
+        this.top = y - this.sY+ this.initTop  <=  - maxT + this.i_top?  - maxT + this.i_top  : y - this.sY + this.initTop ;
+      }else{
+        this.top =   y - this.sY + this.initTop >= maxT + this.i_top ?  maxT + this.i_top:  y - this.sY + this.initTop ;
+      }
+      if(x <= this.sX){
+        this.left = x - this.sX + this.initLeft   <=  -maxL + this.initLeft ?  -maxL + this.initLeft :  x - this.sX + this.initLeft ;
+      }else{
+        this.left = x - this.sX + this.initLeft >=  maxL+ this.initLeft ?  maxL+ this.initLeft :  x - this.sX + this.initLeft ;
+      }
+    },
+    tEventUp(e){
+      document.removeEventListener('mousemove',this.touchmove,false);
+    },
   }
 }
 </script>
@@ -153,14 +341,14 @@ export default {
     position: absolute;
     top: 50%;
     left: 50%;
-    border: 1px solid white;
+    border: 1px solid rgba(255,255,255,0.3);
     margin-left: -500px;
     transform: translateY(-54%);
     .pre_img_con_cur{
       width: 100%;
       height: 600px;
-      display: flex;
-      justify-content: space-around;
+      /*display: flex;*/
+      /*justify-content: space-around;*/
       .icon{
         font-weight: bolder;
         color: #717171;
@@ -173,9 +361,13 @@ export default {
       }
     }
     .pre_img_cur{
+      display: inline-block;
       width: 800px;
       height: 600px;
       overflow: hidden;
+      border: 1px solid white;
+      position: relative;
+      vertical-align: middle;
       img{
         display: inline-block;
         transition:  transform 0.4s ease;
@@ -187,9 +379,10 @@ export default {
       .img_cur_img_w{
         height: 100%;
       }
-      .move{
-        cursor: move;
-      }
+    }
+    .pre_img_cur_move{
+      cursor: move;
+      border: 1px solid red;
     }
   }
   .pre_img_con_fn{
@@ -217,8 +410,8 @@ export default {
     bottom: 100px;
   }
   .pre_img_con_list{
+    min-width: 100%;
     height: 100px;
-    /*border: 1px solid white;*/
     position: absolute;
     display: flex;
     flex-wrap: nowrap;
@@ -231,7 +424,12 @@ export default {
       height: 80px;
       margin-left: 10px;
       margin-top: 10px;
-      border: 1px solid white;
+      border: 1px solid rgba(255,255,255,0.3);
+      position: relative;
+      color: white;
+    }
+    .img_active{
+      border: 1px solid #2178ef;
     }
     .icon{
       display: inline-block;
@@ -241,12 +439,33 @@ export default {
       position: fixed;
       color: white;
       bottom: 0;
+      z-index: 4;
+    }
+    .icon:hover{
+      background: rgba(255,255,255,0.3);
+      color: white;
     }
     .icon-arrow-l{
-      left: 10px;
+      left: 0;
     }
     .icon-arrow-r{
-      right: 10px;
+      right: 0;
+    }
+  }
+  .loading_icon{
+    font-size: 40px;
+    color: white;
+    line-height: 60px;
+    font-weight: normal;
+    animation: rotate 2s ease infinite;
+    transform-origin: 50% 50%;
+  }
+  @keyframes rotate {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
     }
   }
 }
@@ -278,6 +497,9 @@ export default {
 
 .icon-load:before {
   content: "\e904";
+}
+.icon-load1:before {
+  content: "\e912";
 }
 .icon-arrow-b:before {
   content: "\e900";
--- src/components/preview-img.vue
+++ src/components/preview-img.vue
@@ -1,156 +1,168 @@
 <template>
-  <div id="container">
-    <div class="stick"></div>
-    <div class="stick"></div>
-    <div class="stick"></div>
-    <div class="stick"></div>
-    <div class="stick"></div>
-    <div class="stick"></div>
-    <h1>Loading...</h1>
-  </div>
+<div :class="['pre_img_cur']" ref="pre_img_content" @click.stop="select_img" >
+  <img :src="url" alt=""  :style="[img_w_h,trans,preImgCss]" v-show="loaded && url"  ref="preview_img">
+  <template v-if="!loaded">
+    <hw-load v-show="!$slots.load"></hw-load>
+    <div class="pre_img_init_load">
+      <slot name="load"></slot>
+    </div>
+  </template>
+</div>
 </template>
 
 <script>
-
+import hwLoad from "./hw-load.vue"
+export default{
+    components :{hwLoad},
+    data(){return{
+        loaded :false,  //图片是否加载完毕
+        img_w_h : '',
+        w : '', //父容器宽度
+        h : '', //父容易高度
+        i_w : '', //图片本身宽度
+        i_h : '', //图片本身高度
+        initTop : 0, //初始的top值
+        initLeft : 0, //初始的left值
+        top : 0,
+        left : 0
+    }},
+    props:{
+        url : {
+            type : String,
+            default : ''  //显示的图片
+        },
+        load:{
+            type : Boolean,
+            default : true
+        },
+        scale:{
+            type : Number,
+            default : 1
+        },
+        rotate:{
+            type : Number,
+            default : 0
+        },
+        initWidth :{
+           type : Number,
+           default :0
+        },
+        initHeight:{
+            type : Number,
+            default : 0
+        },
+        updatePos:{
+            type : Boolean,
+            default : false
+        },
+        preImgCss:{
+            type : Object,
+            default : function () {
+              return {};
+            }
+        }
+    },
+    computed:{
+      trans(){
+        return {transform : `rotateZ(${this.rotate}deg) scale(${this.scale})`, marginTop : `${this.top}px` , marginLeft : `${this.left}px` };
+      },
+    },
+    methods:{
+      init(v){
+        this.loaded = false;
+        setTimeout(()=>{
+          let image = new Image();
+          image.src =v;
+          image.onload =  ()=> {
+            this.w = parseInt(this.$refs.pre_img_content.offsetWidth);
+            this.h = parseInt(this.$refs.pre_img_content.offsetHeight);
+            let w = this.w;
+            let h = this.h;
+            let i_w = image.width;
+            let i_h = image.height;
+            let s = i_w/i_h;
+            this.i_w = i_w;
+            this.i_h = i_h;
+            this.loaded = true;
+            if(i_w >= w || i_h >= h){
+              if(s >= 1){
+                this.i_w = w;
+                this.i_h = w/s;
+                this.top = (h - w/s)/2;
+                this.img_w_h = {width:'100%'};
+              }else{
+                this.i_h = h;
+                this.i_w = w*s;
+                this.left = (w - h*s)/2;
+                this.img_w_h = {height:'100%'};
+              }
+            }else{
+              this.top = (h - i_h)/2;
+              this.left =(w - i_w)/2;
+            }
+            this.$emit('update:initWidth',this.i_w);
+            this.$emit('update:initHeight',this.i_h);
+            this.$emit('update:i_top',this.top);
+            this.$emit('update:i_left',this.left);
+          }
+        },1000);
+      },
+      select_img(){
+        this.$emit('select',this.url);
+      }
+    },
+    mounted(){
+      this.init(this.url);
+    },
+    watch:{
+        url(v,o){
+            if(v){
+                this.init(v);
+            }
+        },
+        updatePos(v,o){
+            if(v){
+              let top = this.$refs.preview_img.offsetTop;
+              let left = this.$refs.preview_img.offsetLeft;
+              this.$emit('update:initTop',top);
+              this.$emit('update:initLeft',left);
+              this.$emit('update:updatePos',false);
+            }
+      },
+//      preImgCss:{
+//            handler :function (v) {
+//              console.log(v);
+//            },
+//            deep : true
+//      }
+    }
+}
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  $text_color: white;
-  $stick_color: white; //Changes the colour of all sticks
-  $stick_length: 30px; //Changes length of all sticks
-  $stick_distance: -8px; //Changes distance between sticks (will need editing after 30px)
-  @import url(https://fonts.googleapis.com/css?family=Lato:100,300,700);
-  html {
-    background-color:#DC554F;
+.pre_img_cur{
+  position: relative;
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+  .pre_img_init_load{
+    color: white;
+    position: absolute;
+    width: 60px;
+    height: 60px;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%,-50%);
   }
-
-  h1 {
-
-    font-family: 'Lato';
-    color:$text_color;
-    text-transform: uppercase;
-    font-size:1em;
-    letter-spacing:1.5px;
-    text-align:center;
-    width:155px;
-    margin-top:20px;
-    -webkit-animation: fade 2s infinite;
-    -moz-animation: fade 2s infinite;
-
+  .mask{
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    background: black;
+    right: 0;
+    top: 0;
+    position: absolute;
+    z-index: 50;
   }
-
-  #container {
-
-    width:$stick_length*6;
-    padding-top:180px;
-    margin:auto;
-    vertical-align: middle;
-
-  }
-
-  .stick {
-
-    width: $stick_length;
-    height:3px;
-    background:$stick_color;
-    display:inline-block;
-    margin-left:$stick_distance;
-
-  }
-
-  .stick:nth-child(n) {
-    transform:rotate(30deg);
-    -ms-transform:rotate(30deg); /* IE 9 */
-    -webkit-transform:rotate(30deg); /* Safari and Chrome */
-    -moz-transform:rotate(30deg);
-    -webkit-animation: fall 2s infinite;
-    -moz-animation: fall 2s infinite;
-
-  }
-  .stick:nth-child(2n) {
-    transform:rotate(-30deg);
-    -ms-transform:rotate(-30deg); /* IE 9 */
-    -webkit-transform:rotate(-30deg); /* Safari and Chrome */
-    -moz-transform:rotate(-30deg);
-    -webkit-animation: rise 2s infinite;
-    -moz-animation: rise 2s infinite;
-
-  }
-
-  @-webkit-keyframes rise {
-    50% { transform:rotate(30deg);
-      -ms-transform:rotate(30deg); /* IE 9 */
-      -webkit-transform:rotate(30deg);
-      -moz-transform:rotate(30deg);
-    ;}
-  }
-  @-moz-keyframes rise {
-    50% { transform:rotate(30deg);
-      -ms-transform:rotate(30deg); /* IE 9 */
-      -webkit-transform:rotate(30deg);
-      -moz-transform:rotate(30deg);
-    }
-  }
-  @-o-keyframes rise {
-    50% { transform:rotate(30deg);
-      -ms-transform:rotate(30deg); /* IE 9 */
-      -webkit-transform:rotate(30deg);
-      -moz-transform:rotate(30deg);
-    }
-  @keyframes rise {
-    50% { transform:rotate(30deg);
-      -ms-transform:rotate(30deg); /* IE 9 */
-      -webkit-transform:rotate(30deg);
-      -moz-transform:rotate(30deg);
-    }
-  }}
-
-  @-webkit-keyframes fall {
-    50% { transform:rotate(-30deg);
-      -ms-transform:rotate(-30deg); /* IE 9 */
-      -webkit-transform:rotate(-30deg);
-      -moz-transform:rotate(30deg);
-    }
-  }
-  @-moz-keyframes fall {
-    50% { transform:rotate(-30deg);
-      -ms-transform:rotate(-30deg); /* IE 9 */
-      -webkit-transform:rotate(-30deg);
-      -moz-transform:rotate(-30deg);
-
-    }
-  }
-  @-o-keyframes fall {
-    50% { transform:rotate(-30deg);
-      -ms-transform:rotate(-30deg); /* IE 9 */
-      -webkit-transform:rotate(-30deg);
-      -moz-transform:rotate(30deg);
-
-    }
-  @keyframes fall {
-    50% { transform:rotate(-30deg);
-      -ms-transform:rotate(-30deg); /* IE 9 */
-      -webkit-transform:rotate(-30deg);
-      -moz-transform:rotate(30deg);
-
-    }
-  }}
-
-  @-webkit-keyframes fade {
-    50%{opacity:0.5}
-    100%{opacity:1}
-  ;}
-  @-moz-keyframes fade {
-    50%{opacity:0.5}
-    100%{opacity:1}
-  }
-  @-o-keyframes fade {
-    50%{opacity:0.5}
-    100%{opacity:1}
-  @keyframes fade {
-    50%{opacity:0.5}
-    100%{opacity:1}
-  }}
-
+}
 </style>
